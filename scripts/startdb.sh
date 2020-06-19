ead6014d4b7c006e2f400a80b47d4b032fdcaf5a
Vinay Viswanadha <vvinay97@gmail.com>
2020-06-19 16:56:07 -0400

    Init db on database container start

--- scripts/startdb.sh
+++ scripts/startdb.sh
@@ -3,5 +3,6 @@
 port=$(cat db.config.json | jq -r '.port')
 dataDir=$(cat db.config.json | jq -r '.dataDir')
 password=$(cat db.config.json | jq -r '.password')
+db=$(cat db.config.json | jq -r '.database')
 
-docker run --rm --name postgres -p $port:5432 -v $dataDir:/var/lib/postgresql/data -e POSTGRES_PASSWORD=$password -d postgres:13-alpine
+docker run --rm --name postgres -p $port:5432 -v $dataDir:/var/lib/postgresql/data -e POSTGRES_PASSWORD=$password -e POSTGRES_DB=$db -d postgres:13-alpine
